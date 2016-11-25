3609686df484004bb457f1a33df9d0e032a26c66
kharismadiah <kharismadiah@gmail.com>
2016-11-25 10:15:54 +0700

    Update home.php

--- home.php
+++ home.php
@@ -6,6 +6,15 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="https://several-icons/css/fontello.css">
 <body>
+  
+  <!-- Tulisan PSM pojok kiri atas -->
+  <div class="w3-top">
+  <ul class="w3-navbar w3-white w3-wide w3-padding-8 w3-card-2">
+    <li>
+      <a href="#home" class="w3-margin-left"><b>PSM ITS</b></a>
+    </li>
+  </ul>
+  </div>
 
 </html>
 </body>
