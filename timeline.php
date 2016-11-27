5391f9cda0976aa91c6952537ca4542b61e18194
kharismadiah <kharismadiah@gmail.com>
2016-11-27 18:42:01 +0700

    Create timeline.php

---
+++ timeline.php
@@ -0,0 +1,13 @@
+<html>
+<title>W3.CSS Template</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="https://several-icons/css/fontello.css">
+<body>
+
+
+
+</body>
+</html>
