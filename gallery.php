f67ec2ee88385200e5d25ced6210ece4e47c91cc
kharismadiah <kharismadiah@gmail.com>
2016-11-28 17:23:07 +0700

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -29,6 +29,28 @@
   </ul>
 </div>  
 
+<!-- Landing Page -->
+<header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
+  <img class="background-image" src="Assets/body.jpg"  width="100%" height="100%">
+  <div class="w3-display-middle w3-margin-top w3-center">
+    <h1 class="w3-xxlarge w3-animate-zoom"><span class="w3-border w3-border-black w3-padding">PSM</span> <span class="w3-hide-small">Gallery</span></h1>
+  </div>
+<style>
+  body,h1 {font-family: "aramis", aramis}
+</style>
+</header>
+
+<!-- Content -->
+<div class="w3-content w3-padding" style="max-width:1564px">
+  <div class="w3-container w3-padding-32 w3-center" id="about">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-12">Let's explore our world.</h3>
+  </div>
+  <style>
+    .mySlides {display:none}
+    .w3-left, .w3-right, .w3-badge {cursor:pointer}
+    .w3-badge {height:13px;width:13px;padding:0}
+  </style>
+</div>
   
   
   
