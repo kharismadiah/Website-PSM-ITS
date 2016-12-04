74a5457ab8dbdd1be0da0367a4d4b82e2aa05c46
Muhammad Farchan Ramadhan <farchan1998@gmail.com>
2016-12-04 19:13:14 +0700

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -11,17 +11,17 @@
 <div class="w3-top">
   <ul class="w3-navbar w3-white w3-wide w3-padding-8 w3-card-2">
     <li>
-      <a href="#home" class="w3-margin-left"><b>PSM ITS</b></a>
+      <a href="home.php" class="w3-margin-left"><b>PSM ITS</b></a>
     </li>
     <li class="w3-right w3-hide-small">
-      <a href="#home" class="w3-left">Home</a>
-      <a href="#aboutUs" class="w3-left">About Us</a>
-      <a href="#timeline" class="w3-left">Timeline</a>
-      <a href="#articles" class="w3-left">Articles</a>
-      <a href="#gallery" class="w3-left">Gallery</a>
-      <a href="#merchandise" class="w3-left">Merchandise</a>
-      <a href="#reservation" class="w3-left">Reservation</a>
-      <a href="#contact" class="w3-left w3-margin-right">Contact</a>
+      <a href="home.php" class="w3-left">Home</a>
+      <a href="aboutUs.php" class="w3-left">About Us</a>
+      <a href="timeline.php" class="w3-left">Timeline</a>
+      <a href="articles.php" class="w3-left">Articles</a>
+      <a href="gallery.php" class="w3-left">Gallery</a>
+      <a href="merchandise.php" class="w3-left">Merchandise</a>
+      <a href="reservation.php" class="w3-left">Reservation</a>
+      <a href="contact.php" class="w3-left w3-margin-right">Contact</a>
     </li>
     <style>
       body,a {font-family: "aramis", aramis}
