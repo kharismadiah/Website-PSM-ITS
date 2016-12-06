689c4cc06d7c50e58390077bbc0098a8ea1e6ca9
kharismadiah <kharismadiah@gmail.com>
2016-12-06 14:09:21 +0700

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -33,7 +33,8 @@
 <header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
   <img class="background-image" src="Assets/body.jpg"  width="100%" height="100%">
   <div class="w3-display-middle w3-margin-top w3-center">
-    <h1 class="w3-xxlarge w3-animate-zoom"><span class="w3-border w3-border-black w3-padding">PSM</span> <span class="w3-hide-small">Gallery</span></h1>
+    <img src="Assets/logog.png" alt="Logo" style="width:60%">
+    <h1 class="w3-xxlarge w3-animate-zoom"><span class="w3-hide-small">Gallery</span></h1>
   </div>
   <style>
     body,h1 {font-family: "aramis", aramis}
