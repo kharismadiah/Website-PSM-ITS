7b800cffbfd4d6bd9551cd095ae3b0021c5c3f65
Irshad19 <ninimaimi@gmail.com>
2016-12-10 13:48:16 +0700

    <title>Paduan Suara Mahasiswa ITS</title>

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <html>
-<title>W3.CSS Template</title>
+<title>Paduan Suara Mahasiswa ITS</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
