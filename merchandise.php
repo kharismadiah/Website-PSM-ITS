7a06b502c5450d08e3183e180c965b1eb157f83f
kharismadiah <kharismadiah@gmail.com>
2016-12-12 17:43:00 +0700

    Update merchandise.php

--- merchandise.php
+++ merchandise.php
@@ -10,10 +10,9 @@
 <!-- Tulisan PSM pojok kiri atas & Menu di atas -->
 <div class="w3-top">
   <ul class="w3-navbar w3-white w3-wide w3-padding-8 w3-card-2">
-    <li>
-      <a href="home.php" class="w3-margin-left"><b>PSM ITS</b></a>
+    <li class="w3-row-padding">
+	  <img class="logo-image" src="Assets/logoKecil.jpg">
     </li>
-    <!-- Float links to the right. Hide them on small screens -->
     <li class="w3-right w3-hide-small">
       <a href="home.php" class="w3-left">Home</a>
       <a href="articles.php" class="w3-left">Articles</a>
@@ -31,9 +30,9 @@
 
 <!-- Landing Page -->
 <header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
-  <img class="background-image" src="Assets/body.jpg"  width="100%" height="100%">
+  <img class="background-image" src="Assets/body1.png"  width="100%" height="100%">
   <div class="w3-display-middle w3-margin-top w3-center">
-    <img src="Assets/logom.png" alt="Logo" style="width:60%">
+	<img src="Assets/logom.png" alt="Logo" style="width:60%">
     <h1 class="w3-xxlarge w3-animate-zoom"><span class="w3-hide-small">Merchandise</span></h1>
   </div>
   <style>
@@ -44,41 +43,37 @@
 <!-- Content -->
 <!-- Merchandise Section -->
   <div class="w3-container w3-padding-32 w3-center" id="merchandise">
-    <h3 class="w3-border-bottom w3-border-light-grey">Choose the best one.</h3>
+    <h3 class="w3-border-bottom w3-border-light-grey"><font size="5">Choose the best one.</font></h3>
     <p></p>
   </div>
 
-  <div class="w3-row-padding w3-grayscale">
+  <div class="w3-row-padding">
     <div class="w3-col l3 m6 w3-margin-bottom">
-      <img src="Assets/dfdfdfdf.jpg" alt="John" style="width:100%">
-      <h3>Gantungan kunci</h3>
-      <p class="w3-opacity">CEO & Founder</p>
-      <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
-      <p><button class="w3-btn-block">Detail</button></p>
-    </div>
+      <img src="Assets/merchandise.png" alt="John" style="width:100%">
+      <h3><font size="4">Gantungan kunci</font></h3>
+      <p class="w3-opacity"><font size="3">Rp. 15.000</font></p>
+      </div>
     <div class="w3-col l3 m6 w3-margin-bottom">
-      <img src="Assets/dfdfdfdf.jpg" alt="Jane" style="width:100%">
-      <h3>PIN</h3>
-      <p class="w3-opacity">Architect</p>
-      <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
-      <p><button class="w3-btn-block">Detail</button></p>
-    </div>
+      <img src="Assets/merchandise.png" alt="Jane" style="width:100%">
+      <h3><font size="4">PIN</font></h3>
+      <p class="w3-opacity"><font size="3">Rp. 10.000</font></p>
+      </div>
     <div class="w3-col l3 m6 w3-margin-bottom">
-      <img src="Assets/dfdfdfdf.jpg" alt="Mike" style="width:100%">
-      <h3>Sticker</h3>
-      <p class="w3-opacity">Architect</p>
-      <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
-      <p><button class="w3-btn-block">Detail</button></p>
-    </div>
+      <img src="Assets/merchandise.png" alt="Mike" style="width:100%">
+      <h3><font size="4">Sticker</font></h3>
+      <p class="w3-opacity"><font size="3">Rp. 5.000</font></p>
+      </div>
     <div class="w3-col l3 m6 w3-margin-bottom">
-      <img src="Assets/dfdfdfdf.jpg" alt="Dan" style="width:100%">
-      <h3>Kaos</h3>
-      <p class="w3-opacity">Architect</p>
-      <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
-      <p><button class="w3-btn-block">Detail</button></p>
-    </div>
+      <img src="Assets/merchandise.png" alt="Dan" style="width:100%">
+      <h3><font size="4">Kaos</font></h3>
+      <p class="w3-opacity"><font size="3">Rp. 35.000</font></p>
+      </div>
   </div>
-</div>
+  
+  <div class="w3-container w3-padding-32 w3-center" id="merchandise">
+    <h3 class="w3-border-bottom w3-border-light-grey"><font size="5">Contact Person : </font></h3>
+    <p></p>
+  </div>
 
 <!-- Footer -->
 <footer class="w3-center w3-black w3-padding-16">
