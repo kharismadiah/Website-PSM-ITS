92c18ab6ab70b6deeda5f7dcf992321518058f8a
Muhammad Farchan Ramadhan <farchan1998@gmail.com>
2016-12-14 00:16:36 +0700

    Add files via upload

---
+++ september.php
@@ -0,0 +1,59 @@
+<html>
+<title>Paduan Suara Mahasiswa ITS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="https://several-icons/css/fontello.css">
+<style>
+    body,a,h1 {font-family: "aramis", aramis}
+</style>
+<body>
+
+<!-- Tulisan PSM pojok kiri atas & Menu di atas -->
+<div class="w3-top">
+  <ul class="w3-navbar w3-white w3-wide w3-padding-8 w3-card-2">
+    <li class="w3-row-padding">
+      <img class="logo-image" src="Assets/logoKecil.jpg">
+    </li>
+    <li class="w3-right w3-hide-small">
+      <a href="home.php" class="w3-left">Home</a>
+      <a href="articles.php" class="w3-left">Articles</a>
+      <a href="gallery.php" class="w3-left">Gallery</a>
+      <a href="timeline.php" class="w3-left">Timeline</a>
+      <a href="merchandise.php" class="w3-left">Merchandise</a>
+      <a href="reservation.php" class="w3-left">Reservation</a>
+      <a href="aboutUs.php" class="w3-left">About Us</a>
+    </li>
+  </ul>
+</div>
+
+  <!-- About -->
+<div class="w3-container w3-padding-32 w3-center" id="about">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-12"><font size="5">September</font></h3>
+    <p><font size="4">Event 1</font>
+    </p>
+    <p><font size="4">Event 2</font>
+    </p>
+    
+    
+    <p><font size="4">Event 3</font>
+    </p>
+  </div>
+    
+</div>
+
+<!-- Footer -->
+<footer class="w3-center w3-black w3-padding-16">
+  <a href="#" class="w3-btn w3-padding w3-black w3-margin"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
+  <div class="w3-xlarge w3-section">
+    <a href="https://www.facebook.com/itsstudentchoir"><i class="fa fa-facebook-official w3-hover-text-indigo"></i></a>
+    <a href="https://instagram.com/itschoir"><i class="fa fa-instagram w3-hover-text-purple"></i></a>
+    <a href="https://twitter.com/itschoir"><i class="fa fa-twitter w3-hover-text-light-blue" ></i></a>
+    <i class="fa fa-line-messenger w3-hover-text-purple"></i>
+  </div>
+  <p>LINE : <a href="http://line.me/ti/p/~ozp3651p" title="Line PSM ITS" target="_blank" class="w3-hover-text-green">ozp3651p</a></p>
+</footer>
+
+</body>
+</html>
