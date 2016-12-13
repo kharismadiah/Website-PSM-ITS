d9a46fc7047dc63282408ecdad5030b16e90f5cc
Muhammad Farchan Ramadhan <farchan1998@gmail.com>
2016-12-13 09:41:17 +0700

    Update articles.php

--- articles.php
+++ articles.php
@@ -40,22 +40,13 @@
 <!-- Content -->
 <div class="w3-content w3-padding" style="max-width:1564px">
   
+
   <!-- About -->
 <div class="w3-container w3-padding-32 w3-center" id="about">
     <h3 class="w3-border-bottom w3-border-light-grey w3-padding-12"><font size="5">PSM ITS GOES TO WEST JAVA 2016</font></h3>
     <p><font size="4">Studi Banding merupakan salah satu program kerja Departemen External Affair Paduan Suara Mahasiswa Institut Teknologi Sepuluh Nopember (PSM ITS) yang merupakan salah satu bentuk kegiatan yang dilaksanakan guna memperkaya pengetahuan PSM ITS seputar kegiatan dalam dunia Paduan Suara lainnya.</font>
     </p>
-    <p><font size="4">PSM ITS sendiri sempat kedatangan tamu dari teman-teman Paduan Suara Mahasiswa Fakultas Kesehatan Masyarakat Universitas Diponegoro Semarang (PSM FKM Undip) pada Nopember 2014. Pada kesempatan kali ini, PSM ITS yang berada di wilayah Timur Pulau Jawa ingin mengenal lebih dekat seperti apa kehidupan teman-teman Paduan Suara yang ada di wilayah Barat Pulau Jawa dengan berkunjung ke universitas-universitas tersebut. Setelah sebelumnya PSM ITS sempat berkunjung ke Paduan Suara Mahasiswa Institut Teknologi Bandung (ITB) berbarengan dengan kegiatan FPS ITB XXIV , kali ini PSM ITS berkunjung ke tiga PSM universitas. PSM UI Paragita (Universitas Indonesia), PSM Unpad (Universitas Padjajaran), dan PSM Unpar (Universitas Parahyangan). PSM ITS sendiri merupakan Unit Kegiatan Mahasiswa yang berada di bawah naungan sebuah Institut. Sehingga ingin mengetahui lebih jauh mengenai kehidupan paduan suara mahasiswa di universitas. Lebih jauh mengenai rutinitas latihan, penerimaan anggota baru, prestasi, program kerja, serta kegiatan-kegiatan pendukung lainnya yang dapat mempererat hubungan anggota secara internal dari masing-masing paduan suara mahasiswa tersebut tersebut.</font>
-    </p>
-    <img src="Assets/PSMITS&ParagitaUI.jpg" width="50%" height="65%">
-    <p class="w3-opacity"><font size="4">PSM ITS & Paragita UI</font></p>
-    <img src="Assets/PSMITS&PSMUnpar.jpg" width="50%" height="65%">
-    <p class="w3-opacity"><font size="4">PSM ITS & PSM Unpar</font></p>
-    <img src="Assets/PSMITS&PSMUnpad.jpg" width="50%" height="65%">
-    <p class="w3-opacity"><font size="4">PSM ITS & PSM Unpad</font></p>
-    <p><font size="4">Kegiatan ini dilaksanakan pada saat libur awal semester genap, yaitu pada 27-31 Januari 2016. Dengan mengusung tagline “Expanding Relations, Collaborating Harmonies”. PSM ITS ingin memperluas relasi dengan sesama organisasi d bidang paduan suara mahasiswa paduan serta berkolaborasi menyanyikan beberapa lagu guna menambah keakraban suasana kegiatan Studi Banding yang pada akhirnya diharapkan mampu memberi kesan tersendiri bagi masing-masing Paduan Suara Mahasiswa.</font>
-    </p>
-    <p><font size="4">Dengan adanya kegiatan Studi Banding ini, diharapkan PSM ITS dapat mengambil hal-hal positif berupa ilmu dalam memanajemen suatu organisasi paduan suara mahasiswa dalam bidang musikalitas maupun manajerial. Hal ini kemudian dibagikan kepada teman-teman PSM ITS yang tidak mengikuti kegiatan Studi Banding. Sehingga, seluruh anggota PSM ITS mampu merasakan manfaat yang sarat dari kegiatan ini.
+    <p><a href='artikel1.php'>Read More...</a>
     </p>
 </div>
 
@@ -63,20 +54,13 @@
     <h3 class="w3-border-bottom w3-border-light-grey w3-padding-12"><font size="5">Rangkaian Acara Calon LA 2016</font></h3>
     <p><font size="4">Januari merupakan awal bulan yang selalu ditunggu-tunggu oleh setiap organisasi sebagai awal langkah perubahan, begitu pula dengan PSM ITS yang menanti kehadiran para generasi muda untuk PSM ITS. Pada bulan Januari ini terdapat dua kegiatan yang setiap tahun selalu diadakan dan menjadi rutinitas untuk kegiatan di PSM ITS, agenda dan proker tersebut yaitu Pre Competisi dan Latihan Alam 2016.</font>
     </p>
-    <p><font size="4">Mengenai Pre Kompetisi atau dikenal sebagai precomp ini dilaksanakan pada Selasa, 5 Januari 2016 dengan ketua pelaksana yaitu Anisa Ayu Muttaqina. Menurut Ayumu untuk precomp kali ini sedikit berbeda dari pada tahun 2015 kemarin, dimana untuk kali ini hanya ada 3 kelompok yang terbentuk yaitu Tigriz, Ruby dan Moonstone. Lagu yang dinyanyikan pun juga berbeda dari tahun sebelumnya dimana ada lagu Renaissance yang menjadi lagu wajib yaitu Lo Mi Son Giovinetta, dan lagu folklore yang setiap kelompok mendapatkan lagu yang berbeda satu sama lain. Untuk kelompok Tigrizz membawakan lagu Gai Bintang oleh Budi Susanto Yohanes, kelompok Ruby membawakan lagu Janger oleh Bagus Syafrieza P dan kelompok Moonstone membawakan lagu Kicir-kicir oleh Bagus Syafriedza P. Precomp ini merupakan kegiatan yang bisa dikatakan sebagai gladi bersih untuk acara festival di Latihan alam (LA) 2016 sehingga urutan penampilannya pun sesuai dengan ketentuan yang sudah ada.</font>
+      <a href='artikel2.php'>Read More...</a>
     </p>
-    <img src="Assets/LA.jpg" width="50%" height="65%">
-    <p class="w3-opacity"><font size="4">Latihan Alam 2016</font></p>
-    <p><font size="4">Selanjutnya ada LA atau Latihan Alam 2016 yang juga merupakan agenda rutin bagi PSM ITS untuk merekrut anggota baru, yang menjadi Ketua Pelaksana untuk kegiatan ini yaitu Radimas Ananta Putra. Latihan Alam 2016 berlangsung selama 3 hari 2 malam, di Villa Cahaya – Pacet. Suasana baru dan sejuk membuat peserta lebih fresh selama mengikuti kegiatan LA. Bukan hanya warga yang turut hadir, namun ada alumni yang selalu mendukung kegiatan PSM ITS. Menurut Radimas, tentu Latihan Alam 2016 sedikit berbeda dengan Latihan Alam sebelumnya Karena LA kali ini konsepan acara bukan hanya Festival saja namun juga ada sedikit sentuhan materi dimana materi tersebut seperti penambahan wawasan baru bagi para peserta Latihan Alam 2016. Mengenai Festival menurut Radimas tentu para peserta Latihan Alam 2016 yang telah terbagi kedalam 3 kelompok besar yaitu Tigriz, Ruby dan Moonstone telah memberikan yang terbaik dan mengalami perkembangan yang bagus sekali dari sebelum-sebelumnya.
-    </p>
-    <p><font size="4">Oleh karena itu, sebagai tanda apresiasi atas perkembangan yang terjadi kepada peserta Latihan Alam 2016 maka terdapat beberapa penghargaan yang diberikan dengan hasil yang didapatkan perolehan nilai pada saat festival LA tidak terlampau jauh satu sama lain. Berikut daftar juara festival LA 2016 :
-    </p>
-    <img src="Assets/JuaraFestivalLA2016.png" width="50%" height="65%">
-    <p><font size="4">Dengan demikian, juara umum pada Festival LA 2016 adalah kelompok Tigrizz. Dan setelah Latihan Alam diadakan maka PSM ITS sudah memiliki generasi penerus yang baru dimana disebut dengan LA 2016. Latihan Alam merupakan titik nol perjuangan, jadi jangan dulu berbangga atas keikutsertaan dalam acara ini. Masih banyak perjuangan sebenarnya di depan sana, jadi tetap semangat dan tetaplah berjuang bersama di PSM ITS!
-    </p>
+   
   </div>
 	
 </div>
+	
 
 <!-- Footer -->
 <footer class="w3-center w3-black w3-padding-16">
