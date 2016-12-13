6f7e96bbfceab9a45f5014288947736f68624205
kharismadiah <kharismadiah@gmail.com>
2016-12-14 01:55:16 +0700

    Update aboutUs.php

--- aboutUs.php
+++ aboutUs.php
@@ -11,7 +11,7 @@
 <div class="w3-top">
   <ul class="w3-navbar w3-white w3-wide w3-padding-8 w3-card-2">
     <li class="w3-row-padding">
-      <img class="logo-image" src="Assets/logoKecil.jpg">
+	  <img class="logo-image" src="Assets/logoKecil.jpg">
     </li>
     <li class="w3-right w3-hide-small">
       <a href="home.php" class="w3-left">Home</a>
@@ -30,9 +30,9 @@
 
 <!-- Landing Page -->
 <header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
-  <img class="background-image" src="Assets/body1.jpg"  width="100%" height="100%">
+  <img class="background-image" src="Assets/body1.png"  width="100%" height="100%">
   <div class="w3-display-middle w3-margin-top w3-center">
-    <img src="Assets/logoas.png" alt="Logo" style="width:60%">
+	<img src="Assets/logoas.png" alt="Logo" style="width:60%">
     <h1 class="w3-xxlarge w3-animate-zoom"><span class="w3-hide-small">About Us</span></h1>
   </div>
   <style>
@@ -44,48 +44,124 @@
 <div class="w3-content w3-padding" style="max-width:1564px">
   <!-- History -->
   <div class="w3-container w3-center" id="history">
-    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-12"><font size="5">History</font></h3>
-    <p><font size="3">History berisi tentang sejarah awal mula terbentuknya Paduan Suara Mahasiswa ITS dan perkembangannya hingga saat ini..</font></p>
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-12"><font size="5">Sejarah</font></h3>
+    <p><font size="4">ITS Student Choir didirikan pada tahun 1975 oleh Drs. Kresnayana Yahya dan beberapa sarjana ITS lainnya, dengan tujuan membentuk komunitas untuk menampung mahasiswa yang tertarik dengan bidang menyanyi dan paduan suara.</font></p>
+	<p><font size="4">Paduan Suara Mahasiswa Institut Teknologi Sepuluh Nopember (resmi PSMITS, Paduan Suara Mahasiswa ITS) adalah unit kegiatan mahasiswa yang berada di bawah Lembaga Minat Bakat. Dalam beberapa tahun terakhir, ITS Student Choir telah berjuang untuk membuktikan eksistensi mereka dalam dunia paduan suara.</font></p>
   </div>
   <!-- Organigram -->
   <div class="w3-container w3-center" id="organigram">
     <h3 class="w3-border-bottom w3-border-light-grey w3-padding-12"><font size="5">Organigram</font></h3>
-    <p><font size="3">Organigram berisi tentang susunan kepengurusan Paduan Suara Mahasiswa ITS periode 2016/2017..</font></p>
+    <img src="Assets/org.png">
+    <p class="w3-opacity"><font size="4">Susunan Kepengurusan Paduan Suara Mahasiswa ITS 2015/2016</font></p>
   </div>
   <!-- Cundoctor&Coaches -->
   <div class="w3-container w3-center" id="CundoctorAndCoaches">
-    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-12"><font size="5">Conductors and Coaches</font></h3>
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-12"><font size="5">Pelatih & Dirijen</font></h3>
   </div>
   <div class="w3-row-padding w3-grayscale">
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <img src="Assets/dfdfdfdf.jpg" alt="John" style="width:100%">
-      <h3><font size="4">John Doe</font></h3>
-      <p class="w3-opacity"><font size="4">CEO and Founder</font></p>
-      <p><font size="3">Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</font></p>
+    <div class="w3-col m2 w3-margin-bottom">
+	  <div class="w3-display-container">
+      <img src="Assets/Theis Watopa.jpg" alt="John" style="width:100%">
+      <h3><font size="4">Theis Watopa</font></h3>
+      <p class="w3-opacity"><font size="4">Pelatih</font></p>
+	  </div>
     </div>
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <img src="Assets/dfdfdfdf.jpg" alt="John" style="width:100%">
-      <h3><font size="4">John Doe</font></h3>
-      <p class="w3-opacity"><font size="4">CEO and Founder</font></p>
-      <p><font size="3">Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</font></p>
+    <div class="w3-col m2 w3-margin-bottom">
+	  <div class="w3-display-container">
+      <img src="Assets/andreas.jpeg" alt="John" style="width:100%">
+      <h3><font size="4">Andreas Widiatmoko</font></h3>
+      <p class="w3-opacity"><font size="4">Pelatih</font></p>
+	  </div>
     </div>
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <img src="Assets/dfdfdfdf.jpg" alt="John" style="width:100%">
-      <h3><font size="4">John Doe</font></h3>
-      <p class="w3-opacity"><font size="4">CEO and Founder</font></p>
-      <p><font size="3">Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</font></p>
+    <div class="w3-col m2 w3-margin-bottom">
+	  <div class="w3-display-container">
+      <img src="Assets/bagus.PNG" alt="John" style="width:100%">
+      <h3><font size="4">Bagus Syafrieza Paradhika</font></h3>
+      <p class="w3-opacity"><font size="4">Pelatih</font></p>
+	  </div>
     </div>
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <img src="Assets/dfdfdfdf.jpg" alt="Dan" style="width:100%">
-      <h3><font size="4">John Doe</font></h3>
-      <p class="w3-opacity"><font size="4">CEO and Founder</font></p>
-      <p><font size="3">Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</font></p>
+    <div class="w3-col m2 w3-margin-bottom">
+	  <div class="w3-display-container">
+      <img src="Assets/Richard Samuel Awuy.JPG" alt="John" style="width:100%">
+      <h3><font size="4">Richard Samuel Awuy</font></h3>
+      <p class="w3-opacity"><font size="4">Pelatih</font></p>
+	  </div>
+    </div>
+	<div class="w3-col m2 w3-margin-bottom">
+	  <div class="w3-display-container">
+      <img src="Assets/annas.jpeg" alt="John" style="width:100%">
+      <h3><font size="4">Annas Dwi Satriyo</font></h3>
+      <p class="w3-opacity"><font size="4">Pelatih</font></p>
+	  </div>
+    </div>
+	<div class="w3-col m2 w3-margin-bottom">
+	  <div class="w3-display-container">
+      <img src="Assets/Budi Susanto Yohanes.jpg" alt="John" style="width:100%">
+      <h3><font size="4">Budi Susanto Yohanes</font></h3>
+      <p class="w3-opacity"><font size="4">Pelatih</font></p>
+	  </div>
     </div>
   </div>
   <!-- Achievement -->
   <div class="w3-container w3-padding-32 w3-center" id="achievement">
     <h3 class="w3-border-bottom w3-border-light-grey w3-padding-12"><font size="5">Achievement</font></h3>
-    <p><font size="3">Berbentuk timeline yang berisi penghargaan-penghargaan yang telah didapatkan oleh PSM ITS dari waktu ke waktu</font></p>
+    <style>
+    .mySlides {display:none}
+    .w3-left, .w3-right, .w3-badge {cursor:pointer}
+    .w3-badge {height:13px;width:13px;padding:0}
+  </style>
+  <body>
+  <div class="w3-content w3-display-container" style="max-width:500px">
+    <img class="mySlides" src="Assets/2000 2003.png" style="width:100%">
+    <img class="mySlides" src="Assets/2008 2009.png" style="width:100%">
+    <img class="mySlides" src="Assets/2010.png" style="width:100%">
+	<img class="mySlides" src="Assets/2011.png" style="width:100%">
+	<img class="mySlides" src="Assets/2013.png" style="width:100%">
+	<img class="mySlides" src="Assets/2014 2015.png" style="width:100%">
+	<img class="mySlides" src="Assets/2015a.png" style="width:100%">
+	<img class="mySlides" src="Assets/2015b.png" style="width:100%">
+    <div class="w3-center w3-section w3-large w3-text-black w3-display-bottommiddle" style="width:100%">
+      <div class="w3-left w3-padding-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
+      <div class="w3-right w3-padding-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
+      <span class="w3-badge demo w3-border w3-hover-black" onclick="currentDiv(1)"></span>
+      <span class="w3-badge demo w3-border w3-hover-black" onclick="currentDiv(2)"></span>
+      <span class="w3-badge demo w3-border w3-hover-black" onclick="currentDiv(3)"></span>
+	  <span class="w3-badge demo w3-border w3-hover-black" onclick="currentDiv(4)"></span>
+	  <span class="w3-badge demo w3-border w3-hover-black" onclick="currentDiv(5)"></span>
+	  <span class="w3-badge demo w3-border w3-hover-black" onclick="currentDiv(6)"></span>
+	  <span class="w3-badge demo w3-border w3-hover-black" onclick="currentDiv(7)"></span>
+	  <span class="w3-badge demo w3-border w3-hover-black" onclick="currentDiv(8)"></span>
+    </div>
+  </div>
+  <script>
+    var slideIndex = 1;
+    showDivs(slideIndex);
+    function plusDivs(n) {
+      showDivs(slideIndex += n);
+    }
+    function currentDiv(n) {
+      showDivs(slideIndex = n); 
+    }
+    function showDivs(n) {
+      var i;
+      var x = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("demo");
+      if (n > x.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = x.length}
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-white", "");
+      }
+      x[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " w3-white";
+    }
+  </script>
+  <div class="w3-container w3-padding-32" id="about">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-12"></h3>
+  </div>
+ </body>
   </div>
 </div>
 
